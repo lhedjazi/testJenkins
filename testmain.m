e319e543d06c2f86196bc1f74e8db700a139e54e
lhedjazi <hedjazil@hotmail.com>
2019-03-21 16:24:22 +0100

    Test
    Test

---
+++ testmain.m
@@ -0,0 +1,15 @@
+function testmain() 
+% Create a runner that runs completely silently but still produces a valid TestResult 
+runner = matlab.unittest.TestRunner.withNoPlugins; 
+
+% OR 
+
+% Create a runner that produces familiar text output. 
+% runner = matlab.unittest.TestRunner.withTextOutput; 
+
+
+result = runner.run(testsuite('solverTest.m')); 
+rt = table(result); 
+sortrows(rt, 'Duration'); 
+writetable(rt, 'TestResults.csv', 'QuoteStrings', true); 
+end 
